9b489c5a555c19fe0113016856377a9b1b9a465c
leeli <leeli@leelis-iMac-2.local>
2019-08-02 19:37:28 +0800

    admin stuct period 1

--- components/admin/app/controllers/admin/application_controller.rb
+++ components/admin/app/controllers/admin/application_controller.rb
@@ -2,6 +2,22 @@ module Admin
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    before_action :authenticate_user!
+    before_action :authenticate_admin!
+
+    # before_action :check_user
+
+    private
+    def check_user
+      if current_admin
+        flash.clear
+        # if you have rails_admin. You can redirect anywhere really
+        # redirect_to(rails_admin.dashboard_path) && return
+      elsif current_user
+        flash.clear
+        # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+        # redirect_to(authenticated_user_root_path) && return
+      end
+    end
+
   end
 end
--- components/admin/config/routes.rb
+++ components/admin/config/routes.rb
@@ -1,10 +1,26 @@
 Admin::Engine.routes.draw do
   scope path: :admin do
     resources :conferences
+    resources :orders
 
     root to: "conferences#index"
   end
 
-  devise_for :users, module: 'devise', class_name: "Account::User"
+  scope path: "/user", module: "user" do
+    resources :orders
+
+    root to: "orders#index"
+  end
+
+  # namespace :user do
+  #
+  #     resources :orders
+  #     # root to: "conferences#index"
+  #
+  # end
+
+
+  devise_for :users,  module: 'devise', class_name: "Account::User"
+  devise_for :admins, module: 'devise', class_name: "Account::Admin"
 
 end
