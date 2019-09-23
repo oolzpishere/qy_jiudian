e67df9094fa2c84e6f9defaaf3b1dc70cc16c144
leeli <leeli@leelis-iMac-2.local>
2019-09-23 19:24:00 +0800

    change order index phone to room_type_zh

--- components/admin/app/controllers/admin/manager/orders_controller.rb
+++ components/admin/app/controllers/admin/manager/orders_controller.rb
@@ -192,7 +192,7 @@ module Admin
       # copy from permit.
       # at most 4 columns
       def set_show_page_attributes
-        @show_page_attributes = [:group, :names, :contact, :phone, :room_count_zh, :check_in_out, :nights]
+        @show_page_attributes = [:group, :names, :contact, :room_count_zh, :room_type_zh, :check_in_out, :nights]
       end
 
       def set_show_attributes
